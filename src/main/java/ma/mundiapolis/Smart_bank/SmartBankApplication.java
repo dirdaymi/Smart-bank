d68ff79c991ee5ceadc1405a1bdc19cca3a87025
dirdaymi <benlouki235@gmail.com>
2025-12-29 21:21:16 +0100

    Creation de projets et de ses dependances

---
+++ src/main/java/ma/mundiapolis/Smart_bank/SmartBankApplication.java
@@ -0,0 +1,13 @@
+package ma.mundiapolis.Smart_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/mundiapolis/Smart_bank/SmartBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mundiapolis.Smart_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
